2f6e68564d76757883e3e923b6f4dc7f74204cea
lee000000 <licheng@utexas.edu>
2016-12-08 05:25:15 -0600

    Intersection of Two Arrays: use python method runtime average

---
+++ 349.py
@@ -0,0 +1,28 @@
+'''
+349. Intersection of Two Arrays
+
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.
+'''
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1).intersection(nums2))
+
+
+if __name__ == "__main__":
+    nums1 = [1]
+    nums2 = [1, 2]
+    sol = Solution()
+    print(sol.intersection(nums1, nums2))
+    
