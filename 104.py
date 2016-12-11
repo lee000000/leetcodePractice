cae320c056615b42ca1cf6a3cf238de0b4af98a4
lee000000 <licheng@utexas.edu>
2016-12-11 11:31:22 -0600

    max depth of tree: use recursion, greatly improve runtime, better than average

--- 104.py
+++ 104.py
@@ -20,6 +20,30 @@ class Solution(object):
         """
         if not root:
             return 0
+
+        return self.maxDepthH(root, 0)
+
+
+    def maxDepthH(self, root, h):
+        if not root:
+            return h
+
+        # if root.left and not root.right:
+        #     return self.maxDepthH(root.right, h + 1)
+        # elif root.right and not root.left:
+        #     return self.maxDepthH(root.left, h + 1)
+        # else:
+        return max(self.maxDepthH(root.right, h + 1), self.maxDepthH(root.left, h + 1))
+
+
+
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
         height = {root:0,}
         level = [root]
 
