45b5609a75eb894b1d205309d9b8e5f313a22728
lee000000 <licheng@utexas.edu>
2016-12-11 08:25:28 -0600

    The Famous Invert Binary Tree: runtime good, if in base case use 'return' instead of 'return None', runtime average

---
+++ 226.py
@@ -0,0 +1,41 @@
+'''
+226. Invert Binary Tree
+
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), b
+
+ut you can’t invert a binary tree on a whiteboard so fuck off.
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
