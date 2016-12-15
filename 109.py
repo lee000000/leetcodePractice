ce9573878768b8b7d173b46ff3106151806739b2
lee000000 <licheng@utexas.edu>
2016-12-15 15:05:38 -0600

    Convert Sorted List to BST: OJ server problem, runtime actually good

--- 109.py
+++ 109.py
@@ -5,12 +5,6 @@ Given a singly linked list where elements are sorted in ascending order,
 
 convert it to a height balanced BST.
 [1, 2, 3, 4, 5, 6, 7]
-
-
-
-    3
-  2
-1
 '''
 # Definition for singly-linked list.
 # class ListNode(object):
