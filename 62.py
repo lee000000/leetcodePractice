3ef2bb2a3172e16825f39d2ef9be5b91b8abe1dd
lee000000 <licheng@utexas.edu>
2016-12-21 09:36:30 -0600

    Unique Path: DP, runtime good

---
+++ 62.py
@@ -0,0 +1,52 @@
+'''
+62. Unique Paths
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start'
+
+in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot
+
+is trying to reach the bottom-right corner of the grid (marked 'Finish'
+
+in the diagram below).
+
+How many possible unique paths are there?
+
+start [] [] [] [] [] []
+[]    [] [] [] [] [] []
+[]    [] [] [] [] [] finish
+
+Above is a 3 x 7 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+'''
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 0 or n == 0:
+            return 0
+        dp = [[0] * (n)] * m
+        dp[0][0] = 1
+        # dp[0][1] = dp[1][0] = 1
+        for i in range(0, m):
+            for j in range(1, n):
+                # print((i, j))
+                #print((dp[i-1][j],  dp[i][j - 1]))
+                dp[i][j] = dp[i-1][j] + dp[i][j - 1]
+
+            # print(dp[i])
+        return dp[m - 1][n - 1]
+
+
+def test():
+    sol = Solution()
+    print(sol.uniquePaths(3, 7))
+
+
+if __name__ == "__main__":
+    test()
