7d86f1ec416484dc2d10838727980084d3556e0e
lee000000 <licheng@utexas.edu>
2016-12-21 12:24:02 -0600

    Unique Path: should have skipped the upper left corner

--- 62.py
+++ 62.py
@@ -32,7 +32,7 @@ class Solution(object):
             return 0
         dp = [[0 for _ in range(n)] for _ in range(m)]
         dp[0][0] = 1
-    
+
 
 
         for i in range(m):
@@ -41,7 +41,13 @@ class Solution(object):
                 # #print((dp[i-1][j],  dp[i][j - 1]))
                 # print((dp[i][j],  dp[i-1][j], dp[i][j - 1]))
 
-                dp[i][j] += dp[i-1][j] + dp[i][j - 1]
+                if i == 0:
+
+                    dp[i][j] += dp[i][j - 1]
+                elif j == 0:
+                    dp[i][j] += dp[i - 1][j]
+                else:
+                    dp[i][j] += dp[i - 1][j] + dp[i][j - 1]
 
         # for line in dp:
         #     print(line)
