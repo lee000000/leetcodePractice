226451fc99c457fa77670717079f7488de6d271c
lee000000 <licheng@utexas.edu>
2016-12-22 13:47:34 -0600

    Triangle: top-down DP, runtime average

--- 120.py
+++ 120.py
@@ -10,7 +10,7 @@ For example, given the following triangle
      [2],
     [3,4],
    [6,5,7],
-  [4,-1,8,3]
+  [4,1,8,3]
 ]
 The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
 
@@ -21,7 +21,30 @@ is the total number of rows in the triangle.
 '''
 import sys
 class Solution(object):
+    # naive DP
+    # top-down
     def minimumTotal(self, triangle):
+
+        minSum = [[0 for i in range(len(row))] for row in triangle]
+        minSum[0][0] = triangle[0][0]
+        # print(minSum)
+        for i in range(1, len(triangle)):
+            for j in range(len(triangle[i])):
+                # print((i, j ))
+                if j == 0:
+                    minSum[i][j] = minSum[i - 1][j] + triangle[i][j]
+                elif j == len(triangle[i]) - 1:
+                    minSum[i][j] = minSum[i - 1][j - 1] + triangle[i][j]
+                else:
+                    minSum[i][j] = min(minSum[i - 1][j - 1], minSum[i - 1][j] ) + triangle[i][j]
+
+        return min(minSum[-1])
+
+
+    # runtime good
+    # bottom-up
+    # O(n) space complexity
+    def minimumTotal_best(self, triangle):
         """
         :type triangle: List[List[int]]
         :rtype: int
