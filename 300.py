67c8ec79ff5540ebd10ee5cf2a78c9d943ac2826
lee000000 <licheng@utexas.edu>
2016-12-27 11:02:21 -0600

    Longest Increasing Subsequence: add O(NlogN) method, DP + binary search

--- 300.py
+++ 300.py
@@ -41,12 +41,59 @@ class Solution(object):
 
         return maxSoFar
 
+    # From GeeksForGeeks: O(NlogN)
+    def lengthOfLISBinary(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # endArray keeps track of the largest element of the
+        # active arrays starting with each num in nums
+        endArray = [0 for _ in range(len(nums))]
+        endArray[0] = nums[0]
+        endI = 1 # always points to the next emplty ending element's position
+        for i in range(1, len(nums)):
+            if nums[i] < endArray[0]: # nums[i] is less than the smallest
+                # start a new array
+                endArray[0] = nums[i]
+            elif nums[i] > endArray[endI - 1]: # nums[i] is larger than the biggest
+                # extend the end array
+                endArray[endI] = nums[i]
+                endI += 1
+            else:
+                # find the index of the ceil element for nums[i]
+                index = self.ceilIndex(endArray, -1, endI - 1, nums[i])
+                # replace the ending element with the new one
+                endArray[index] = nums[i]
+        #print(endArray)
+        return endI
+
+
+    def ceilIndex(self, array, l, r, key):
+        # binary search for key
+        while l < r - 1:
+            print((l, r))
+            m = l + (r - l) // 2
+            if array[m] < key:
+                l = m
+            else:
+                r = m
+        return r
+
 
 def test():
-    nums = [10, 9, 2, 5, 3, 7, 101, 18]
-    numsb = [1,3,6,7,9,4,10,5,6]
+    numsa = [10, 9, 2, 5, 3, 7, 101, 18]
+    numsb = [2, 5, 3, 7, 11, 8, 10, 13, 6] # --> 6
+    nums = [4,10,4,3,8,9]
     sol = Solution()
-    print(sol.lengthOfLIS(nums))
+    print(sol.lengthOfLISBinary(nums))
+
+def testBinary():
+    a = [1, 1, 3, 5, 6]
+    l = 0
+    r = len(a) - 1
+    sol = Solution()
+    print(sol.ceilIndex(a, l , r, 4))
 
 
 if __name__ == "__main__":
